4d262b74b317757ff112983c89c35114b988c050
HP Laptop <isibaksh@gamil.com>
2022-12-02 07:31:24 +0000

    Day 2 adevent code

---
+++ Advent2022/Days/Day2.cs
@@ -0,0 +1,54 @@
+﻿namespace Advent2022.Days;
+
+internal class Day2
+{
+    enum Hands
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+
+    enum Outcome
+    {
+        Win = 6,
+        Draw = 3,
+        Loss = 0
+    }
+
+    private static Hands GetHands(char input) => input switch
+    {
+        'A' or 'X' => Hands.Rock,
+        'B' or 'Y' => Hands.Paper,
+        'C' or 'Z' => Hands.Scissors,
+        _ => throw new ArgumentException(),
+    };
+
+
+    private static Outcome GetRoundsResult(char player1, char player2)
+    {
+        if (player1 == player2)
+        {
+            return Outcome.Draw;
+        }
+
+        if (GetHands(player1) + 1 == GetHands(player2))
+        {
+            return Outcome.Win;
+        }
+    }
+
+    public static int GetTotalScore()
+    {
+        string input = Helpers.GetInput(2);
+
+        string[] rounds = input.Split("\n\r");
+
+        foreach (string round in rounds)
+        {
+            if (GetHands(round[0]))
+        }
+
+        return 0;
+    }
+}
---
+++ Advent2022/Helpers.cs
@@ -0,0 +1,6 @@
+﻿namespace Advent2022;
+
+internal class Helpers
+{
+    public static string GetInput(int day) => File.ReadAllText($"./Assets/Day{day}_Input.txt");
+}
