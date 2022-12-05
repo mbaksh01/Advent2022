65801be62e577bfe34d5d670cace1d1484e11064
Mohammed Baksh <mbaksh@redev.co.uk>
2022-12-05 07:13:37 +0000

    day 5 template

---
+++ Advent2022/Days/Day5.cs
@@ -0,0 +1,21 @@
+﻿namespace Advent2022.Days;
+
+internal class Day5
+{
+    internal void LoadStacks(string[] crates)
+    {
+
+    }
+
+    internal void MoveCrates(int fromStack, int toStack, int count)
+    {
+
+    }
+
+    public string GetCrateOrder()
+    {
+        string input = Helpers.GetInput(5);
+
+        return string.Empty;
+    }
+}
