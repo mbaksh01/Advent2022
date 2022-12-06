dd8a42e880b0ba40e224eb89068aa9efeb9a0765
Moe <mbaksh@redev.co.uk>
2022-12-06 09:03:01 +0000

    tests everywhere

--- Advent2022.Tests.Unit/Days/Day6Tests.cs
+++ Advent2022.Tests.Unit/Days/Day6Tests.cs
@@ -7,12 +7,33 @@ public class Day6Tests
     [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
     [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
     [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
-    public void GetStartOfPackatMarkerTest(string dataStream, int index)
+    public void IdentifyUniqueSection_Length4(string dataStream, int index)
     {
         // Act
-        int response = Day6.GetStartOfPackatMarker(dataStream);
+        int response = Day6.IdentifyUniqueSection(dataStream, 4);
 
         // Assert
         _ = response.Should().Be(index);
     }
+
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    public void IdentifyUniqueSection_Length14(string dataStream, int index)
+    {
+        // Act
+        int response = Day6.IdentifyUniqueSection(dataStream, 14);
+
+        // Assert
+        _ = response.Should().Be(index);
+    }
+
+    [Fact]
+    public void GetStartOfPackatMarkerTest() => _ = Day6.GetStartOfPackatMarker().Should().Be(1542);
+
+    [Fact]
+    public void GetStartOfMessageMarkerTest() => _ = Day6.GetStartOfMessageMarker().Should().Be(3153);
 }
