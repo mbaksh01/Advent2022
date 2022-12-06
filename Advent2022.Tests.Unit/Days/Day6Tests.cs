0cfa190c9e3f4827192a11a5db2538b37f28a715
Mohammed Baksh <mbaksh@redev.co.uk>
2022-12-06 06:52:38 +0000

    added tests and day 6 template

---
+++ Advent2022.Tests.Unit/Days/Day6Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace Advent2022.Tests.Unit.Days;
+
+public class Day6Tests
+{
+    [Theory]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void GetStartOfPackatMarkerTest(string dataStream, int index)
+    {
+        // Act
+        int response = Day6.GetStartOfPackatMarker(dataStream);
+
+        // Assert
+        _ = response.Should().Be(index);
+    }
+}
---
+++ Advent2022/Days/Day6.cs
@@ -0,0 +1,16 @@
+﻿namespace Advent2022.Days;
+
+internal class Day6
+{
+    public static int GetStartOfPackatMarker()
+    {
+        string input = Helpers.GetInput(6);
+
+        return GetStartOfPackatMarker(input);
+    }
+
+    internal static int GetStartOfPackatMarker(ReadOnlySpan<char> dataStream)
+    {
+        return 0;
+    }
+}
